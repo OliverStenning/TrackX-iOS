157c9774f04b5c4e9433c376f4aacb5d9ba91aae
Oliver Stenning <oliver@stenning.co>
2022-03-25 16:41:00 +0000

    Initial Commit

---
+++ TrackXUITests/TrackXUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrackXUITestsLaunchTests.swift
+//  TrackXUITests
+//
+//  Created by Oliver Stenning on 25/03/2022.
+//
+
+import XCTest
+
+class TrackXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
