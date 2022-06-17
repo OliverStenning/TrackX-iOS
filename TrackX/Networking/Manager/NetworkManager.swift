7b79610c352b4cd422cee033cc58e3b5115b6b5a
Oliver Stenning <oliver@stenning.co>
2022-06-17 22:47:59 +0100

    Refactored network code with generics

--- TrackX/Networking/Manager/NetworkManager.swift
+++ TrackX/Networking/Manager/NetworkManager.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 let PRINT_ERROR = false
+let PRINT_RESPONSE = false
+let PRINT_JSON = false
 
 enum NetworkResponse:String {
     case success
@@ -48,297 +50,93 @@ struct NetworkManager {
         }
     }
     
-    // MARK: - Launch functions
+    func handleResponse<T: Decodable>(_ data: Data?, _ response: URLResponse?, _ error: Error?, completion: @escaping (_ launches: T?, _ error: String?) -> ()) {
+        if error != nil {
+            completion(nil, "Please check your network connection.")
+        }
+        
+        if let response = response as? HTTPURLResponse {
+            let result = self.handleNetworkResponse(response)
+            switch result {
+            case .success:
+                guard let responseData = data else {
+                    completion(nil, NetworkResponse.noData.rawValue)
+                    return
+                }
+                do {
+                    if PRINT_RESPONSE {
+                        print(responseData)
+                    }
+                    if PRINT_JSON {
+                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                        print(jsonData)
+                    }
+                    let apiResponse = try JSONDecoder().decode(T.self, from: responseData)
+                    completion(apiResponse, nil)
+                } catch {
+                    if PRINT_ERROR {
+                        print(error)
+                    }
+                    completion(nil, NetworkResponse.unableToDecode.rawValue)
+                }
+            case .failure(let networkFailureError):
+                completion(nil, networkFailureError)
+            }
+        }
+    }
     
+    // MARK: - Launch functions
     func getLaunches(completion: @escaping (_ launches: [Launch]?, _ error: String?) -> ()) {
         launchRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-                        //print(responseData)
-                        //let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-                        //print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Launch].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     func getNextLaunch(completion: @escaping (_ launch: Launch?, _ error: String?) -> ()) {
         launchRouter.request(.next) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode(Launch.self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Rocket functions
-    
     func getRockets(completion: @escaping (_ rockets: [Rocket]?, _ error: String?) -> ()) {
         rocketRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Rocket].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Launchpad functions
-    
     func getLaunchpads(completion: @escaping (_ launchpads: [Launchpad]?, _ error: String?) -> ()) {
         launchpadRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Launchpad].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Landpad functions
-    
     func getLandpads(completion: @escaping (_ landpads: [Landpad]?, _ error: String?) -> ()) {
         landpadRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Landpad].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Core functions
-    
     func getCores(completion: @escaping (_ cores: [Core]?, _ error: String?) -> ()) {
         coreRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Core].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Capsule functions
-    
     func getCapsules(completion: @escaping (_ capsules: [Capsule]?, _ error: String?) -> ()) {
         capsuleRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Capsule].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
     // MARK: - Payload functions
-    
     func getPayloads(completion: @escaping (_ payloads: [Payload]?, _ error: String?) -> ()) {
         payloadRouter.request(.all) { data, response, error in
-            
-            if error != nil {
-                completion(nil, "Please check your network connection.")
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                let result = self.handleNetworkResponse(response)
-                switch result {
-                case .success:
-                    guard let responseData = data else {
-                        completion(nil, NetworkResponse.noData.rawValue)
-                        return
-                    }
-                    do {
-//                        print(responseData)
-//                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-//                        print(jsonData)
-                        let apiResponse = try JSONDecoder().decode([Payload].self, from: responseData)
-                        completion(apiResponse, nil)
-                    } catch {
-                        if PRINT_ERROR {
-                            print(error)
-                        }
-                        completion(nil, NetworkResponse.unableToDecode.rawValue)
-                    }
-                case .failure(let networkFailureError):
-                    completion(nil, networkFailureError)
-                }
-            }
-            
+            handleResponse(data, response, error, completion: completion)
         }
     }
     
