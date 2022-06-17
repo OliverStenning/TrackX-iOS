a1beab6686c3b86444a5de2b122e80f5de418d68
Oliver Stenning <oliver@stenning.co>
2022-06-18 00:52:40 +0100

    Refactored networking code for clarity

--- TrackX/Networking/Manager/NetworkManager.swift
+++ TrackX/Networking/Manager/NetworkManager.swift
@@ -40,104 +40,43 @@ struct NetworkManager {
     private let capsuleRouter = Router<CapsuleApi>()
     private let payloadRouter = Router<PayloadApi>()
     
-    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
-        switch response.statusCode {
-        case 200...299: return .success
-        case 401...500: return .failure(NetworkResponse.authenticationError.rawValue)
-        case 501...599: return .failure(NetworkResponse.badRequest.rawValue)
-        case 600: return .failure(NetworkResponse.outdated.rawValue)
-        default: return .failure(NetworkResponse.failed.rawValue)
-        }
-    }
-    
-    func handleResponse<T: Decodable>(_ data: Data?, _ response: URLResponse?, _ error: Error?, completion: @escaping (_ launches: T?, _ error: String?) -> ()) {
-        if error != nil {
-            completion(nil, "Please check your network connection.")
-        }
-        
-        if let response = response as? HTTPURLResponse {
-            let result = self.handleNetworkResponse(response)
-            switch result {
-            case .success:
-                guard let responseData = data else {
-                    completion(nil, NetworkResponse.noData.rawValue)
-                    return
-                }
-                do {
-                    if PRINT_RESPONSE {
-                        print(responseData)
-                    }
-                    if PRINT_JSON {
-                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
-                        print(jsonData)
-                    }
-                    let apiResponse = try JSONDecoder().decode(T.self, from: responseData)
-                    completion(apiResponse, nil)
-                } catch {
-                    if PRINT_ERROR {
-                        print(error)
-                    }
-                    completion(nil, NetworkResponse.unableToDecode.rawValue)
-                }
-            case .failure(let networkFailureError):
-                completion(nil, networkFailureError)
-            }
-        }
-    }
-    
     // MARK: - Launch functions
     func getLaunches(completion: @escaping (_ launches: [Launch]?, _ error: String?) -> ()) {
-        launchRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        launchRouter.request(.all, completition: completion)
     }
     
     func getNextLaunch(completion: @escaping (_ launch: Launch?, _ error: String?) -> ()) {
-        launchRouter.request(.next) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        launchRouter.request(.next, completition: completion)
     }
     
     // MARK: - Rocket functions
     func getRockets(completion: @escaping (_ rockets: [Rocket]?, _ error: String?) -> ()) {
-        rocketRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        rocketRouter.request(.all, completition: completion)
     }
     
     // MARK: - Launchpad functions
     func getLaunchpads(completion: @escaping (_ launchpads: [Launchpad]?, _ error: String?) -> ()) {
-        launchpadRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        launchpadRouter.request(.all, completition: completion)
     }
     
     // MARK: - Landpad functions
     func getLandpads(completion: @escaping (_ landpads: [Landpad]?, _ error: String?) -> ()) {
-        landpadRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        landpadRouter.request(.all, completition: completion)
     }
     
     // MARK: - Core functions
     func getCores(completion: @escaping (_ cores: [Core]?, _ error: String?) -> ()) {
-        coreRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        coreRouter.request(.all, completition: completion)
     }
     
     // MARK: - Capsule functions
     func getCapsules(completion: @escaping (_ capsules: [Capsule]?, _ error: String?) -> ()) {
-        capsuleRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        capsuleRouter.request(.all, completition: completion)
     }
     
     // MARK: - Payload functions
     func getPayloads(completion: @escaping (_ payloads: [Payload]?, _ error: String?) -> ()) {
-        payloadRouter.request(.all) { data, response, error in
-            handleResponse(data, response, error, completion: completion)
-        }
+        payloadRouter.request(.all, completition: completion)
     }
     
 }
--- TrackX/Networking/Service/Router.swift
+++ TrackX/Networking/Service/Router.swift
@@ -7,19 +7,64 @@
 
 import Foundation
 
-class Router<EndPoint: EndPointType>: NetworkRouter {
+class Router<EndPoint: EndPointType> {
     private var task: URLSessionTask?
     
-    func request(_ route: EndPoint, completition: @escaping NetworkRouterCompletion) {
+    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
+        switch response.statusCode {
+        case 200...299: return .success
+        case 401...500: return .failure(NetworkResponse.authenticationError.rawValue)
+        case 501...599: return .failure(NetworkResponse.badRequest.rawValue)
+        case 600: return .failure(NetworkResponse.outdated.rawValue)
+        default: return .failure(NetworkResponse.failed.rawValue)
+        }
+    }
+    
+    func handleResponse<T: Decodable>(_ data: Data?, _ response: URLResponse?, _ error: Error?, completion: @escaping (_ launches: T?, _ error: String?) -> ()) {
+        if error != nil {
+            completion(nil, "Please check your network connection.")
+        }
+        
+        if let response = response as? HTTPURLResponse {
+            let result = self.handleNetworkResponse(response)
+            switch result {
+            case .success:
+                guard let responseData = data else {
+                    completion(nil, NetworkResponse.noData.rawValue)
+                    return
+                }
+                do {
+                    if PRINT_RESPONSE {
+                        print(responseData)
+                    }
+                    if PRINT_JSON {
+                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                        print(jsonData)
+                    }
+                    let apiResponse = try JSONDecoder().decode(T.self, from: responseData)
+                    completion(apiResponse, nil)
+                } catch {
+                    if PRINT_ERROR {
+                        print(error)
+                    }
+                    completion(nil, NetworkResponse.unableToDecode.rawValue)
+                }
+            case .failure(let networkFailureError):
+                completion(nil, networkFailureError)
+            }
+        }
+    }
+    
+    func request<T: Decodable>(_ route: EndPoint, completition: @escaping (_ data: T?, _ error: String?) -> ()) {
         let session = URLSession.shared
         do {
             let request = try self.buildRequest(from: route)
             NetworkLogger.log(request: request)
             task = session.dataTask(with: request, completionHandler: { data, response, error in
-                completition(data, response, error)
+                self.handleResponse(data, response, error, completion: completition)
             })
         } catch {
-            completition(nil, nil, error)
+            self.handleResponse(nil, nil, nil, completion: completition)
         }
         self.task?.resume()
     }
