b97fc428a33ed72dd65dbb9d09df31a453276198
Oliver Stenning <oliver@stenning.co>
2023-02-08 00:22:04 +0000

    Added Utilities shared package

---
+++ Shared/Utilities/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utilities",
+    platforms: [.iOS(.v14)],
+    products: [.library(name: "Utilities", type: .dynamic, targets: ["Utilities"])],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Utilities",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "UtilitiesTests",
+            dependencies: ["Utilities"]
+        )
+    ]
+)
